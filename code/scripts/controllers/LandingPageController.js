6b82b89c10036c02291b6b5d8825b53331beabc2
paiusCatalin <p.cata95@gmail.com>
2021-11-24 10:53:09 +0200

    updated landing page

--- code/scripts/controllers/LandingPageController.js
+++ code/scripts/controllers/LandingPageController.js
@@ -1,11 +1,10 @@
 const {WebcController} = WebCardinal.controllers;
-import CommunicationService from '../../services/CommunicationService.js';
-import ResponsesService from '../../services/ResponsesService.js';
-import TrialParticipantRepository from '../../repositories/TrialParticipantRepository.js';
-import TrialRepository from '../../repositories/TrialRepository.js';
-import IotAdaptorApi from "../../services/IotAdaptorApi.js";
+import CommunicationService from '../services/CommunicationService.js';
+import ResponsesService from '../services/ResponsesService.js';
+import TrialParticipantRepository from '../repositories/TrialParticipantRepository.js';
+import TrialRepository from '../repositories/TrialRepository.js';
 
-export default class HomeController extends WebcController {
+export default class LandingPageController extends WebcController {
     constructor(element, history) {
         super(element, history);
 
