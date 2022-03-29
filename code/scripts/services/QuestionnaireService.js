862c7f3ff735f2a383dde07efeac8f379c7d5b80
nliappas <67898132+nliappas@users.noreply.github.com>
2022-03-29 13:07:38 +0200

    QuestionnaireService.js

---
+++ code/scripts/services/QuestionnaireService.js
@@ -0,0 +1,20 @@
+const commonServices = require("common-services");
+const DSUService = commonServices.DSUService;
+
+export default class QuestionnaireService extends DSUService {
+
+    constructor() {
+        super('/questionnaires');
+    }
+
+    mount = (keySSI, callback) => this.mountEntity(keySSI, callback);
+
+    getAllQuestionnaires = (callback) => this.getEntities(callback);
+
+    getQuestionnaire = (uid, callback) => this.getEntity(uid, callback);
+
+    saveQuestionnaire = (data, callback) => this.saveEntity(data, callback);
+
+    updateQuestionnaire = (data, callback) => this.updateEntity(data, callback);
+
+}
