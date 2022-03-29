b7cc298f59dca40e6c79f4c4e5e2b40eb5b5c15d
Cosmin Tuca <tuc@rms.ro>
2022-03-29 12:57:11 +0300

    solve conflicts

--- code/scripts/controllers/econsent/TrialManagementController.js
+++ code/scripts/controllers/econsent/TrialManagementController.js
@@ -1,43 +1,6 @@
 import HCOService from '../../services/HCOService.js';
 const {WebcController} = WebCardinal.controllers;
 const { DataSource } = WebCardinal.dataSources;
-
-class StudiesDataSource extends DataSource {
-    constructor(...props) {
-        super(...props);
-        this.model.trials = props[0];
-        this.model.elements = 10;
-        this.setPageSize(this.model.elements);
-        this.model.noOfColumns = 4;
-    }
-
-    updateData(updatedStudy) {
-        let toBeUpdatedIndex = this.model.trials.findIndex(study => updatedStudy.uid === study.uid);
-        this.model.studies[toBeUpdatedIndex] = updatedStudy;
-        this.forceUpdate(true);
-    }
-
-    async getPageDataAsync(startOffset, dataLengthForCurrentPage) {
-        console.log({startOffset, dataLengthForCurrentPage});
-        if (this.model.trials.length <= dataLengthForCurrentPage ){
-            this.setPageSize(this.model.trials.length);
-        }
-        else{
-            this.setPageSize(this.model.elements);
-        }
-        let slicedData = [];
-        this.setRecordsNumber(this.model.trials.length);
-        if (dataLengthForCurrentPage > 0) {
-            slicedData = Object.entries(this.model.trials).slice(startOffset, startOffset + dataLengthForCurrentPage).map(entry => entry[1]);
-            console.log(slicedData)
-        } else {
-            slicedData = Object.entries(this.model.trials).slice(0, startOffset - dataLengthForCurrentPage).map(entry => entry[1]);
-            console.log(slicedData)
-        }
-        return slicedData;
-    }
-}
-
 const commonServices = require('common-services');
 const DataSourceFactory = commonServices.getDataSourceFactory();
 
