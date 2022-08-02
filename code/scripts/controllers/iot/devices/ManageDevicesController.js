dd271c64677da99480970e1b1ac58c85920a7bfd
Cosmin Tuca <tuc@rms.ro>
2022-08-02 17:55:41 +0300

    remove allDevices from the model, solve code review comments

--- code/scripts/controllers/iot/devices/ManageDevicesController.js
+++ code/scripts/controllers/iot/devices/ManageDevicesController.js
@@ -49,10 +49,10 @@ export default class ManageDevicesController extends BreadCrumbManager {
     filterData() {
         let searchKeys = ['deviceId', 'modelNumber', 'manufacturer', 'deviceName', 'brand', 'status', 'trial']
 
-        let allDevices = this.model.toObject('allDevices');
+        let devices = this.model.toObject('mappedDevices');
 
         if (this.model.search.value.trim() !== '') {
-            let filteredDevices = allDevices.filter(device => {
+            let filteredDevices = devices.filter(device => {
 
                 let keys = Object.keys(device);
                 for (let key of keys) {
@@ -75,7 +75,7 @@ export default class ManageDevicesController extends BreadCrumbManager {
             }
         }
         else {
-            this.model.devicesDataSource.updateDevices(allDevices);
+            this.model.devicesDataSource.updateDevices(devices);
             this.model.noResults = false;
         }
     }
@@ -87,24 +87,22 @@ export default class ManageDevicesController extends BreadCrumbManager {
             if (err) {
                 return console.error(err);
             }
-            this.model.allDevices = devices.filter(device => device.archived !== true);
+            let allDevices = devices.filter(device => device.archived !== true);
             this.model.allAssignedDevices = devices.filter(dv => dv.isAssigned === true);
-            this.getAssignedDevices();
+            this.getAssignedDevices(allDevices);
         });
 
     }
 
-    getAssignedDevices(){
+    getAssignedDevices(allDevices){
         this.DeviceAssignationService = new DeviceAssignationService();
-        this.DeviceAssignationService.getAssignedDevices( (err, assignedDevices) => {
+        this.DeviceAssignationService.getAssignedDevices( (err, usedDevices) => {
             if (err) {
                 return console.error(err);
             }
-            let devices = this.model.allDevices;
-            this.model.assignedDevices = assignedDevices;
-            this.model.mappedDevices =  devices.map(device => {
+            this.model.mappedDevices =  allDevices.map(device => {
                 if(device.isAssigned === true) {
-                    let assignedDevice = assignedDevices.find(item => item.deviceId === device.deviceId);
+                    let assignedDevice = usedDevices.find(item => item.deviceId === device.deviceId);
                     device.tpNumber = assignedDevice.trialParticipantNumber;
                 }
                 return device;
@@ -112,7 +110,7 @@ export default class ManageDevicesController extends BreadCrumbManager {
 
             this.model.devicesDataSource = DataSourceFactory.createDataSource(7, 5, this.model.toObject('mappedDevices'));
             this.model.devicesDataSource.__proto__.updateDevices = function (devices) {
-                this.model.allDevices = devices;
+                this.model.mappedDevices = devices;
                 this.model.tableData = devices;
                 this.getElement().dataSize = devices.length;
                 this.forceUpdate(true);
@@ -195,10 +193,10 @@ export default class ManageDevicesController extends BreadCrumbManager {
                             });
 
                             this.deviceServices.updateDevice(device, () => {
-                                let removedDeviceIdx = this.model.allDevices.findIndex(device => device.uid === deviceUid);
+                                let removedDeviceIdx = this.model.mappedDevices.findIndex(device => device.uid === deviceUid);
 
-                                this.model.allDevices.splice(removedDeviceIdx, 1);
-                                this.model.devicesDataSource.updateDevices(this.model.allDevices);
+                                this.model.mappedDevices.splice(removedDeviceIdx, 1);
+                                this.model.devicesDataSource.updateDevices(this.model.mappedDevices);
                                 window.WebCardinal.loader.hidden = true;
                             })
 
